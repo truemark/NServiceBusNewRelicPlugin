3a6c8e38eb0600c3cb8c6bcae0c54bd63966291f
dsisodia30 <dsisodia@netradius.com>
2017-04-19 13:57:26 +0530

    NRP-49: Updated units for metrics and adding new build.

--- NServiceBusNewRelicPlugin/NServiceMetrics.cs
+++ NServiceBusNewRelicPlugin/NServiceMetrics.cs
@@ -13,9 +13,9 @@ namespace NServiceBusNewRelicPlugin
         public NServiceMetrics() {
             //lstNServiceMetrics.Add("Name", new ValuePair { metricName = "Name", metricUnit = "Name" });
             lstNServiceMetrics.Add("CriticalTime", new ValuePair { metricName = "Critical Time", metricUnit = "Sec" });
-            lstNServiceMetrics.Add("NumberofmsgsfailuresPersec", new ValuePair { metricName = "Number Of Messages Failures", metricUnit = "Per/Sec" });
-            lstNServiceMetrics.Add("NumberofmsgspulledfromtheinputqueuePersec", new ValuePair { metricName = "Number Of Messages Pulled From Input Queue", metricUnit = "Per/Sec" });
-            lstNServiceMetrics.Add("NumberofmsgssuccessfullyprocessedPersec", new ValuePair { metricName = "Number Of Messages Fully Processed", metricUnit = "Per/Sec" });
+            lstNServiceMetrics.Add("NumberofmsgsfailuresPersec", new ValuePair { metricName = "Messages/Number Of Messages Failures", metricUnit = "Messages/Sec" });
+            lstNServiceMetrics.Add("NumberofmsgspulledfromtheinputqueuePersec", new ValuePair { metricName = "Messages/Number Of Messages Pulled From Input Queue", metricUnit = "Messages/Sec" });
+            lstNServiceMetrics.Add("NumberofmsgssuccessfullyprocessedPersec", new ValuePair { metricName = "Messages/Number Of Messages Fully Processed", metricUnit = "Messages/Sec" });
             lstNServiceMetrics.Add("SLAviolationcountdown", new ValuePair { metricName = "SLA Voilation Countdown", metricUnit = "Voilations" });
         }
     }
